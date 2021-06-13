09816945ce6deb2f39596f0703fabe4a4d30bf9f
Oleksandr Andreiev <oleksandr.andreiev@spacelabs.com>
2021-06-13 14:18:04 +0300

    tmp

---
+++ src/main/java/com/alex/andreiv/Factors.java
@@ -0,0 +1,18 @@
+package com.alex.andreiv;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Factors {
+
+    public List<Integer> commonFactors(int[] nums){
+        var factorsList = new ArrayList<Integer>();
+        for (int num : nums) {
+            while (num != 1)
+            {
+
+            }
+        }
+        return factorsList;
+    }
+}
