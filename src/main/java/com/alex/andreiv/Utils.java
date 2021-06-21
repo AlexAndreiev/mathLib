51d6345c766f5264dd5ca736c13488b08481433e
Oleksand Andreiev <andreyew89gmail.com>
2021-06-21 07:26:00 +0300

    minor comments have been added

--- src/main/java/com/alex/andreiv/Utils.java
+++ src/main/java/com/alex/andreiv/Utils.java
@@ -6,7 +6,7 @@ public class Utils {
 
 /*
      All primes are of form 6k+i for some integer k and for i = -1,0,1,2,3,4.
-     2  divides (6k+1), (6k+2), (6k+3=4)
+     2 divides (6k+0), (6k+2), (6k+4)
      3 divides (6k+3)
      check if n is divisible by 2 or 3, then to check through all the numbers of form 6k+-1;
 */
@@ -25,6 +25,11 @@ public class Utils {
          since then the product a*b would be greater than sqrt(n)*sqrt(n) = n
          So in any factorization of n, at least one of the factors must be smaller than the square root of n
          if we can't find any factors less than or equal to the square root, n must be a prime
+*/
+/*
+         for better understanding
+        for (int k = 1; 6*k <= Math.sqrt(number); k ++)
+            if (number % (6*k-1) == 0 || number % (6*k+1) == 0) return false;
 */
         for (int i = 5; i * i <= number; i += 6)
             if (number % i == 0 || number % (i+2) == 0) return false;
