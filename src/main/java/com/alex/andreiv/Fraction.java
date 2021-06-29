3c4a6a7784a8e7e0ecc47dc865dd44c47e419d2d
Oleksandr Andreiev <oleksandr.andreiev@spacelabs.com>
2021-06-29 09:14:44 +0300

    Fraction class structure has been added (TODO)

---
+++ src/main/java/com/alex/andreiv/Fraction.java
@@ -0,0 +1,13 @@
+package com.alex.andreiv;
+
+public class Fraction {
+
+//    private byte sign = 1;
+    private long numerator = 0;
+    private long denominator = 0;
+
+    public Fraction(long numerator, long denominator) {
+        this.numerator = numerator;
+        this.denominator = denominator;
+    }
+}
