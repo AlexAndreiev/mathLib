daa14990e52a80decb155eef2c6e5e8ee60a5ffb
Oleksandr Andreiev <andreyew89@gmail.com>
2021-07-04 12:25:20 +0300

    Geom points (#3)
    * Point and Point2D have been added
    
    Co-authored-by: Oleksandr Andreiev <oleksandr.andreiev@spacelabs.com>

---
+++ src/test/java/com/alex/andreiv/geometry/PointTest.java
@@ -0,0 +1,14 @@
+package com.alex.andreiv.geometry;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PointTest {
+
+    @Test
+    public void testGetX() {
+        var point = new Point(2);
+        assertEquals(point.getX(), 2);
+    }
+}
