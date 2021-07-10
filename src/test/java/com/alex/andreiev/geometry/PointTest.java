d6ef970a24b521b0640b9e24e935fe0d9eb37983
Oleksandr Andreiev <oleksandr.andreiev@spacelabs.com>
2021-07-10 14:57:19 +0300

    PointTest.cloneTest and PointTest.equals have been added

--- src/test/java/com/alex/andreiev/geometry/PointTest.java
+++ src/test/java/com/alex/andreiev/geometry/PointTest.java
@@ -10,13 +10,27 @@ public class PointTest {
     public void getX() {
         var point = new Point(2);
         assertEquals(point.getX(), 2);
+
+        point = new Point(-4);
+        assertEquals(point.getX(), -4);
     }
 
     @Test
     void cloneTest() {
+        var point = new Point(2);
+        var clonedPoint = (Point) point.clone();
+        assertEquals(point.getX(), clonedPoint.getX());
+        // make sure references are not equal
+        assertTrue(point != clonedPoint);
     }
 
     @Test
     void equals() {
+        var point1 = new Point(2);
+        var point2 = new Point(2);
+        // make sure references are not equal
+        assertTrue(point1 != point2);
+        // calls equals method
+        assertEquals(point1, point2);
     }
 }
